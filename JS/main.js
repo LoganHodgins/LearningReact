a476e93a96d987f88738ef383976b7f019cdd4a1
Logan Hodgins <loganhodginsofficial@gmail.com>
2022-07-12 23:35:00 -0500

    Added files for part 2

--- JS/main.js
+++ JS/main.js
@@ -47,3 +47,79 @@ console.log(func1(1, 2));
 console.log(func2(1, 2));
 console.log(func3(1));
 console.log(func4());
+
+//------------------------- Arrays ------------------------------
+// Method time complexity:
+//----------- Add / Remove ---------
+// push    | O(1)
+// pop     | O(1)
+// shift   | O(N)
+// unshift | O(N)
+// splice  | O(N)
+// sort    | O(N * log(N))
+//------------ Access -----------
+// concat  | O(N)
+// slice   | O(N)
+// indexof | O(N)
+// ------------ Iteration ----------
+// forEach | O(N)
+// map     | O(N)
+// filter  | O(N)
+// reduce  | O(N)
+
+// two ways to create arrays
+let arr1 = new Array();
+let arr2 = [];
+
+let fruits = ['Apple', 'Orange', 'Plum'];
+
+// arrays can store a mix of elements
+
+// Arrays as queue
+let q = [];
+q.push(1); // add element to end
+q.shift(); // remove and get element from front
+
+// Arrays as stack
+let s = [];
+s.push(12); // add element to end
+s.pop(); // remove and get element from end
+
+s.unshift(12); // add element to front
+
+// both push and unshift can add multiple elements
+// arrays are special objects
+
+let s_copy = s; // copy s by reference
+
+// push/pop are fast
+// shift/unshift are slow
+
+// technically possible since arrays are object
+// will loop through all properties
+for (let key in fruits) {
+    console.log(fruits[key]);
+}
+
+// iterates through all elements
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+// old
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// array length is writable
+let arr3 = [1, 2, 3, 4];
+arr3.length = 1; // remove all but first element
+for (let val of arr3) {
+    console.log(val); // 1
+}
+
+// array .toString() return comma seperated string
+let arr4 = [1, 2, 3, 4];
+
+let bool = arr4 === arr3; // always false
+console.log(bool);
