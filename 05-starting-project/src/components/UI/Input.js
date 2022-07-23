38c260dac9408d8ae950f20466b16a47116e8ceb
Logan Hodgins <loganhodginsofficial@gmail.com>
2022-07-23 00:29:52 -0500

    Added cart context

--- 05-starting-project/src/components/UI/Input.js
+++ 05-starting-project/src/components/UI/Input.js
@@ -1,10 +1,14 @@
+import React from 'react';
+
 import classes from './Input.module.css';
 
-const Input = props => {
+// need to wrap our component with React.forwardRef to
+// allow us to use ref on our custom component.
+const Input = React.forwardRef((props, ref) => {
   return <div className={classes.input}>
     <label htmlFor={props.input.id}>{props.label}</label>
-    <input {...props.input}/>
+    <input ref={ref} {...props.input}/>
   </div>
-};
+});
 
 export default Input;
